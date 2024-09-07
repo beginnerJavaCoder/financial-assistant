0d8cec6704fe94271c5cdc98414ececee5dd3ab2
Eugene <zhenyaesipovm@mail.ru>
2024-09-07 20:15:16 +0400

    Add initial project structure

---
+++ src/main/java/com/assistant/financial/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.assistant.financial.view.user;
+
+import com.assistant.financial.entity.User;
+import com.assistant.financial.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
